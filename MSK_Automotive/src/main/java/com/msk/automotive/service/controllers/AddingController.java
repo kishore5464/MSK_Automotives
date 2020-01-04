1c0471aad8c51dddced1b15a24142b35ed3090a4
Kishore Kumar <kishorekumar5464@gmail.com>
2020-01-04 19:50:28 +0530

    Get Business completed

--- MSK_Automotive/src/main/java/com/msk/automotive/service/controllers/AddingController.java
+++ MSK_Automotive/src/main/java/com/msk/automotive/service/controllers/AddingController.java
@@ -11,8 +11,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,7 +18,6 @@ import org.springframework.web.servlet.ModelAndView;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.msk.automotive.business.interfaces.Get_Business_Interface;
 import com.msk.automotive.business.interfaces.Insert_Business_Interface;
-import com.msk.automotive.business.interfaces.Update_Business_Interface;
 import com.msk.automotive.exception.CustomGenericException;
 import com.msk.automotive.service.pojo.CarBrands_Pojo;
 import com.msk.automotive.service.pojo.CarModels_Pojo;
@@ -40,9 +37,6 @@ public class AddingController {
 
 	@Autowired
 	private Insert_Business_Interface insert_Business_Interface;
-
-	@Autowired
-	private Update_Business_Interface update_Business_Interface;
 
 	@PostMapping(value = "/add-car-brand")
 	public Response add_car_brand(@FormParam("brand") String brand, @FormParam("logo") String logo,
