cf27392aaa06bdc88b5c3b966c02413cd05631b0
Kishore Kumar <kishorekumar5464@gmail.com>
2020-01-09 23:18:05 +0530

    just commited

--- MSK_Automotive/src/main/java/com/msk/automotive/dao/repositories/Location_Repository.java
+++ MSK_Automotive/src/main/java/com/msk/automotive/dao/repositories/Location_Repository.java
@@ -3,6 +3,8 @@ package com.msk.automotive.dao.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.msk.automotive.service.entities.Location;
@@ -10,6 +12,7 @@ import com.msk.automotive.service.entities.Location;
 @Repository
 public interface Location_Repository extends JpaRepository<Location, Integer> {
 
-	public List<Location> findByCity(String city);
+	@Query(value = "SELECT location FROM Location location WHERE location.city = :city")
+	public List<Location> findByCity(@Param(value = "city")String city);
 	
 }
