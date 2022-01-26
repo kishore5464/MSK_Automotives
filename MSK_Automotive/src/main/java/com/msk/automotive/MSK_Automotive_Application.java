711a3131305f20280fb8d9a3098a27a4f17c4010
Kishore Kumar <kishorekumar@Kishores-MacBook-Air.local>
2022-01-27 01:33:11 +0530

    Initial Commit

--- MSK_Automotive/src/main/java/com/msk/automotive/MSK_Automotive_Application.java
+++ MSK_Automotive/src/main/java/com/msk/automotive/MSK_Automotive_Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MSK_Automotive_Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MSK_Automotive_Application.class, args);
 	}
